5c8870bf6a74385400377a6ba555da9f008a786b
Juan Pedro Fisanotti <fisadev@gmail.com>
2022-06-07 02:30:43 -0300

    More winwing settings now in ui layer

--- ui_throttle_ww.diff.lua
+++ ui_throttle_ww.diff.lua
@@ -1,28 +1,20 @@
 local diff = {
 	["keyDiffs"] = {
+		["d2501pnilu2501cdnilvd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN25",
+				},
+			},
+			["name"] = "as left mouse button",
+		},
 		["d2502pnilu2502cdnilvd1vpnilvu0"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "JOY_BTN105",
+					["key"] = "JOY_BTN23",
 				},
 			},
 			["name"] = "as right mouse button",
-		},
-		["d2503pnilu2503cdnilvd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN104",
-				},
-			},
-			["name"] = "as mouse wheel forward",
-		},
-		["d2504pnilu2504cdnilvd1vpnilvu0"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "JOY_BTN103",
-				},
-			},
-			["name"] = "as mouse wheel backward",
 		},
 	},
 }
