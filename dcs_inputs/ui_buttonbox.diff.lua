4044332390e99ad9f388a25742454e4039300774
Juan Pedro Fisanotti <fisadev@gmail.com>
2022-09-15 22:38:54 -0300

    Migrate UI mappings to deck setup

---
+++ dcs_inputs/ui_buttonbox.diff.lua
@@ -0,0 +1,45 @@
+local diff = {
+	["keyDiffs"] = {
+		["d191pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN10",
+				},
+			},
+			["name"] = "Time decelerate",
+		},
+		["d246pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN6",
+				},
+			},
+			["name"] = "Time normal",
+		},
+		["d51pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN1",
+				},
+			},
+			["name"] = "End mission",
+		},
+		["d52pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN5",
+				},
+			},
+			["name"] = "Pause",
+		},
+		["d53pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN2",
+				},
+			},
+			["name"] = "Time accelerate",
+		},
+	},
+}
+return diff
--- dcs_inputs/ui_kbd.diff.lua
+++ dcs_inputs/ui_kbd.diff.lua
@@ -3,183 +3,28 @@ local diff = {
 		["d157pnilunilcdnilvdnilvpnilvunil"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "1",
+					["key"] = "Q",
 					["reformers"] = {
-						[1] = "LCtrl",
+						[1] = "LAlt",
+						[2] = "LCtrl",
+						[3] = "LShift",
 					},
 				},
 			},
 			["name"] = "Frame rate counter - Service info",
-			["removed"] = {
-				[1] = {
-					["key"] = "Pause",
-					["reformers"] = {
-						[1] = "RCtrl",
-					},
-				},
-			},
-		},
-		["d1641pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Restart Mission",
-			["removed"] = {
-				[1] = {
-					["key"] = "R",
-					["reformers"] = {
-						[1] = "LShift",
-					},
-				},
-			},
-		},
-		["d1670pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Multiplayer chat read/write all",
-			["removed"] = {
-				[1] = {
-					["key"] = "Tab",
-					["reformers"] = {
-						[1] = "LShift",
-					},
-				},
-			},
-		},
-		["d1673pnilunilcdnilvdnilvpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "2",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "Chat show/hide",
-			["removed"] = {
-				[1] = {
-					["key"] = "Y",
-					["reformers"] = {
-						[1] = "LCtrl",
-						[2] = "LShift",
-					},
-				},
-			},
-		},
-		["d1686pnilunilcdnilvdnilvpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "3",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "BDA show/hide",
-			["removed"] = {
-				[1] = {
-					["key"] = "'",
-					["reformers"] = {
-						[1] = "RAlt",
-					},
-				},
-			},
-		},
-		["d191pnilunilcdnilvdnilvpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = ",",
-				},
-			},
-			["name"] = "Time decelerate",
-			["removed"] = {
-				[1] = {
-					["key"] = "Z",
-					["reformers"] = {
-						[1] = "LAlt",
-					},
-				},
-			},
 		},
 		["d216pnilunilcdnilvdnilvpnilvunil"] = {
 			["added"] = {
 				[1] = {
-					["key"] = "`",
+					["key"] = "W",
 					["reformers"] = {
-						[1] = "LCtrl",
+						[1] = "LAlt",
+						[2] = "LCtrl",
+						[3] = "LShift",
 					},
 				},
 			},
 			["name"] = "recenter VR Headset",
-		},
-		["d246pnilunilcdnilvdnilvpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "/",
-				},
-			},
-			["name"] = "Time normal",
-			["removed"] = {
-				[1] = {
-					["key"] = "Z",
-					["reformers"] = {
-						[1] = "LShift",
-					},
-				},
-			},
-		},
-		["d50pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Multiplayer chat - mode Allies",
-			["removed"] = {
-				[1] = {
-					["key"] = "Tab",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-		},
-		["d52pnilunilcdnilvdnilvpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = "Esc",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-			["name"] = "Pause",
-		},
-		["d53pnilunilcdnilvdnilvpnilvunil"] = {
-			["added"] = {
-				[1] = {
-					["key"] = ".",
-				},
-			},
-			["name"] = "Time accelerate",
-			["removed"] = {
-				[1] = {
-					["key"] = "Z",
-					["reformers"] = {
-						[1] = "LCtrl",
-					},
-				},
-			},
-		},
-		["d56pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Show VoiceChat window",
-			["removed"] = {
-				[1] = {
-					["key"] = "Tab",
-					["reformers"] = {
-						[1] = "LCtrl",
-						[2] = "LShift",
-					},
-				},
-			},
-		},
-		["d57pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Multiplayer chat - mode All",
-			["removed"] = {
-				[1] = {
-					["key"] = "Tab",
-				},
-			},
 		},
 	},
 }
