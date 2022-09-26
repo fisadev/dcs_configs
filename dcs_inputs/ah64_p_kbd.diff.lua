7cccc06bb77faf08b26b041f95e75f49f66b2dd8
Juan Pedro Fisanotti <fisadev@gmail.com>
2022-09-25 21:53:13 -0300

    Restored default mappings no longer conflicting with my own

--- dcs_inputs/ah64_p_kbd.diff.lua
+++ dcs_inputs/ah64_p_kbd.diff.lua
@@ -1,21 +1,5 @@
 local diff = {
 	["keyDiffs"] = {
-		["d1032pnilu1034cdnilvdnilvpnilvunil"] = {
-			["name"] = "Power Lever Smoothly (Both) - Increase",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num+",
-				},
-			},
-		},
-		["d1033pnilu1034cdnilvdnilvpnilvunil"] = {
-			["name"] = "Power Lever Smoothly (Both) - Decrease",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num-",
-				},
-			},
-		},
 		["d1587pnilunilcdnilvdnilvpnilvunil"] = {
 			["added"] = {
 				[1] = {
@@ -28,46 +12,6 @@ local diff = {
 				},
 			},
 			["name"] = "Kneeboard ON/OFF",
-		},
-		["d177pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Zoom normal",
-			["removed"] = {
-				[1] = {
-					["key"] = "NumEnter",
-				},
-			},
-		},
-		["d17pnilu18cdnilvdnilvpnilvunil"] = {
-			["name"] = "Cockpit panel view in",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num0",
-				},
-			},
-		},
-		["d197pnilu198cdnilvdnilvpnilvunil"] = {
-			["name"] = "Cyclic - Bank LEFT",
-			["removed"] = {
-				[1] = {
-					["key"] = "Left",
-				},
-			},
-		},
-		["d199pnilu200cdnilvdnilvpnilvunil"] = {
-			["name"] = "Cyclic - Bank RIGHT",
-			["removed"] = {
-				[1] = {
-					["key"] = "Right",
-				},
-			},
-		},
-		["d19pnilunilcdnilvdnilvpnilvunil"] = {
-			["name"] = "Lock View (cycle padlock)",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num.",
-				},
-			},
 		},
 		["d3001pnilu3001cd31vd1vpnilvu0"] = {
 			["added"] = {
@@ -95,14 +39,6 @@ local diff = {
 			},
 			["name"] = "Automatic Startup Procedure",
 		},
-		["d74pnilu75cdnilvd1vpnilvu0"] = {
-			["name"] = "Wheel Brake - ON/OFF",
-			["removed"] = {
-				[1] = {
-					["key"] = "W",
-				},
-			},
-		},
 		["d816pnilunilcdnilvdnilvpnilvunil"] = {
 			["added"] = {
 				[1] = {
@@ -128,86 +64,6 @@ local diff = {
 				},
 			},
 			["name"] = "Show controls indicator",
-		},
-		["dnilp210u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Up Right slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num9",
-				},
-			},
-		},
-		["dnilp211u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Down Right slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num3",
-				},
-			},
-		},
-		["dnilp212u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Down Left slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num1",
-				},
-			},
-		},
-		["dnilp213u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Up Left slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num7",
-				},
-			},
-		},
-		["dnilp32u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Left slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num4",
-				},
-			},
-		},
-		["dnilp33u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Right slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num6",
-				},
-			},
-		},
-		["dnilp34u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Up slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num8",
-				},
-			},
-		},
-		["dnilp35u214cdnilvdnilvpnilvunil"] = {
-			["name"] = "View Down slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num2",
-				},
-			},
-		},
-		["dnilp36unilcdnilvdnilvpnilvunil"] = {
-			["name"] = "View Center",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num5",
-				},
-			},
-		},
-		["dnilp39u289cdnilvdnilvpnilvunil"] = {
-			["name"] = "Zoom in slow",
-			["removed"] = {
-				[1] = {
-					["key"] = "Num*",
-				},
-			},
 		},
 	},
 }
