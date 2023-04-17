685ff1494aa829b33f62c8545cbd0b8a2c073143
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-04-17 00:42:35 -0300

    Added kbd mapping to vr recenter to prevent it from being a default that conflicts with the 5 mapping in the apache kb unit

---
+++ dcs_inputs/ui_kbd.diff.lua
@@ -0,0 +1,30 @@
+local diff = {
+	["keyDiffs"] = {
+		["d216pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Down",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "LCtrl",
+						[3] = "LShift",
+					},
+				},
+			},
+			["name"] = "recenter VR Headset",
+		},
+		["d816pnilunilcdnilvdnilvpnilvunil"] = {
+			["name"] = "Active Pause",
+			["removed"] = {
+				[1] = {
+					["key"] = "Pause",
+					["reformers"] = {
+						[1] = "LShift",
+						[2] = "LWin",
+					},
+				},
+			},
+		},
+	},
+}
+return diff
