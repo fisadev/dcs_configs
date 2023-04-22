b3b1ba99af332073169ce72b61683ce3036adc00
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-04-22 19:53:50 -0300

    Added scripts to run m2vjoy, and copy configs and them on import

--- import_configs.sh
+++ import_configs.sh
@@ -1,3 +1,6 @@
 #bash
 cp /c/Users/fisad/Saved\ Games/DCS/InputUserProfiles/*.* /d/flight/dcs_configs/dcs_inputs/
 cp /c/Users/fisad/Desktop/remaps_dcs.ahk /d/flight/dcs_configs/autohotkey_scripts/
+cp /d/installed_aps/midi2vjoy_lpd8/*.bat /d/flight/dcs_configs/midi2vjoy
+cp /d/installed_aps/midi2vjoy_lpd8/*.conf /d/flight/dcs_configs/midi2vjoy
+cp /d/installed_aps/midi2vjoy_lpd8/*.lnk /d/flight/dcs_configs/midi2vjoy
---
+++ midi2vjoy/run.bat
@@ -0,0 +1,3 @@
+d:
+cd installed_apps\midi2vjoy_lpd8
+py midi2vjoy_lpd8.py -m 1 -c my_mappings.conf
