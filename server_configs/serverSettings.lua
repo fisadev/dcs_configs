03d70d94f402dd2752dbbdaaa773631f773f98d5
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-05-01 19:53:09 -0300

    Initial version of the server config

---
+++ server_configs/serverSettings.lua
@@ -0,0 +1,48 @@
+cfg = 
+{
+    ["description"] = "Bienvenidos a Los Magios DCS Server IV. Este servidor es administrado por Los Magios, y es requerimiento estar dentro de nuestro discord mientras se vuele en el servidor: https://discord.gg/S6QqNWvM",
+    ["require_pure_textures"] = true,
+    ["listStartIndex"] = 1,
+    ["advanced"] = 
+    {
+        ["allow_change_tailno"] = false,
+        ["disable_events"] = false,
+        ["allow_ownship_export"] = true,
+        ["allow_object_export"] = false,
+        ["pause_on_load"] = true,
+        ["allow_sensor_export"] = false,
+        ["event_Takeoff"] = true,
+        ["pause_without_clients"] = true,
+        ["client_outbound_limit"] = 0,
+        ["client_inbound_limit"] = 0,
+        ["server_can_screenshot"] = true,
+        ["allow_players_pool"] = false,
+        ["voice_chat_server"] = false,
+        ["allow_change_skin"] = false,
+        ["event_Connect"] = true,
+        ["event_Ejecting"] = true,
+        ["event_Kill"] = true,
+        ["event_Crash"] = true,
+        ["event_Role"] = true,
+        ["resume_mode"] = 2,
+        ["maxPing"] = 0,
+        ["allow_trial_only_clients"] = true,
+        ["allow_dynamic_radio"] = false,
+    }, -- end of ["advanced"]
+    ["port"] = 10308,
+    ["mode"] = 0,
+    ["bind_address"] = "",
+    ["isPublic"] = true,
+    ["listShuffle"] = false,
+    ["password"] = "SET THE PASS",
+    ["listLoop"] = true,
+    ["name"] = "Los Magios DCS Server IV",
+    ["require_pure_scripts"] = true,
+    ["missionList"] = 
+    {
+        [1] = "C:\\Users\\Administrator\\Saved Games\\DCS.openbeta_server\\Missions\\Operation_Nerve_Wrecker_1.1.miz",
+    }, -- end of ["missionList"]
+    ["require_pure_clients"] = true,
+    ["require_pure_models"] = true,
+    ["maxPlayers"] = 20,
+} -- end of cfg
