7ea7e5200942ef6972cb5006883c1e32dc1ce916
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-06-27 22:20:19 -0300

    Update dir name for simpyt configs

--- import_configs.sh
+++ import_configs.sh
@@ -1,4 +1,4 @@
 #bash
 cp /c/Users/fisad/Saved\ Games/DCS/InputUserProfiles/*.* /d/flight/dcs_configs/dcs_inputs/
 cp /c/Users/fisad/Desktop/remaps_dcs.ahk /d/flight/dcs_configs/autohotkey_scripts/
-cp /d/installed_aps/simpyt/configs/* /d/flight/dcs_configs/simpyt_configs/ -r
+cp /d/installed_aps/simpyt/simpyt_configs/* /d/flight/dcs_configs/simpyt_configs/ -r
