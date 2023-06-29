c578e4a9aca8173a2fd86bab772f718d85d530b6
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-06-29 20:25:25 -0300

    Basic almost empty f15 wso mappings, just to be able to fly it in emergencies

---
+++ dcs_inputs/f15_wso_kbd.diff.lua
@@ -0,0 +1,16 @@
+local diff = {
+	["keyDiffs"] = {
+		["d438pnilunilcdnilvdnilvpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "H",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Helmet Visor/NVG Toggle",
+		},
+	},
+}
+return diff
