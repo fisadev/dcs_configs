6ae4c998307e13e80e8e31573392f8f137450d36
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-07-04 21:34:33 -0300

    Dcs script backed up

---
+++ dcs_scripts/Export.lua
@@ -0,0 +1,11 @@
+-- comments
+
+-- srs
+pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Services\DCS-SRS\Scripts\DCS-SimpleRadioStandalone.lua]]); end,nil)
+
+-- simshaker
+-- dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
+-- local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua')
+
+--tacview
+local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
