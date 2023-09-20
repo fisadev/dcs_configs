b7a0f11b7b533eead9512dbf673ce069c27cae0b
Juan Pedro Fisanotti <fisadev@gmail.com>
2023-09-20 20:53:19 -0300

    Just cleaning

--- dcs_scripts/Export.lua
+++ dcs_scripts/Export.lua
@@ -1,11 +1,10 @@
--- comments
+-- simshaker
+dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
+local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua')
+
+--tacview
+local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
 
 -- srs
 pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Services\DCS-SRS\Scripts\DCS-SimpleRadioStandalone.lua]]); end,nil)
 
--- simshaker
--- dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
--- local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua')
-
---tacview
-local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
