e565adb837c4623698907aa89d841cc0c7784c33
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-01-10 01:10:49 -0300

    Backup important open kneeboard configs

--- import_configs.sh
+++ import_configs.sh
@@ -5,3 +5,6 @@ cp /c/Users/fisad/Desktop/remaps_dcs.ahk /d/flight/dcs_configs/autohotkey_script
 cp /d/installed_aps/simpyt/simpyt_configs/* /d/flight/dcs_configs/simpyt_configs/ -r
 cp  "/c/Program Files (x86)/Steam/steamapps/common/DCSWorld/Mods/aircraft/F-15E/Cockpit/Scripts/ALE45_PRG.lua" /d/flight/dcs_configs/f15e_cockpit_scripts
 cp  "/c/Program Files (x86)/Steam/steamapps/common/DCSWorld/Scripts/DemoScenes/sceneVR.lua" /d/flight/dcs_configs/dcs_scripts
+cp "/c/Users/fisad/Saved Games/OpenKneeboard/profiles/default/DirectInput.json" /d/flight/dcs_configs/open_kneeboard_configs
+cp "/c/Users/fisad/Saved Games/OpenKneeboard/profiles/default/Tabs.json" /d/flight/dcs_configs/open_kneeboard_configs
+cp "/c/Users/fisad/Saved Games/OpenKneeboard/profiles/default/VR.json" /d/flight/dcs_configs/open_kneeboard_configs
