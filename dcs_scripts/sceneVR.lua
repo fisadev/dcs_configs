013091e33a4d5039ad4623107ac7c405f10f7408
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-02-23 23:22:30 -0300

    Update scene VR with windows line endings

--- dcs_scripts/sceneVR.lua
+++ dcs_scripts/sceneVR.lua
@@ -1,20 +1,20 @@
 package.path = package.path ..';./Scripts/DemoScenes/?.lua;'
 local sceneEnvironment = require("demosceneEnvironment")
 
-scene = {} 
+scene = {}
 
 function loadScene(scenePtr)
-	sceneAPI = sceneEnvironment.getInterface(scenePtr)
-	sceneAPI:setUpdateFunc('sceneVRUpdate')
+        sceneAPI = sceneEnvironment.getInterface(scenePtr)
+        sceneAPI:setUpdateFunc('sceneVRUpdate')
 
-	local cam_level = 1.8
+        local cam_level = 1.8
 
-	scene.cam		= sceneAPI:addCamera(3, cam_level , 3)
-	scene.cam:setFarClip(1000.0)
-	scene.cam:setFov(90)
-	scene.cam.transform:lookAtPoint(-10.0, cam_level, 0);
+        scene.cam               = sceneAPI:addCamera(3, cam_level , 3)
+        scene.cam:setFarClip(1000.0)
+        scene.cam:setFov(90)
+        scene.cam.transform:lookAtPoint(-10.0, cam_level, 0);
 
-	scene.cam:setActive()
+        scene.cam:setActive()
 end
 
 function sceneVRUpdate(t, dt)
