84e415f1b1301c780552d41e9bce917e5a19cf30
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-05-22 19:45:14 -0300

    Mi24 aim station mappings

---
+++ dcs_inputs/mi24_c_stick_aim_station.diff.lua
@@ -0,0 +1,47 @@
+local diff = {
+	["axisDiffs"] = {
+		["a3057cd7"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.05,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = true,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "Aiming Station Up/Down (Joystick)",
+		},
+		["a3058cd7"] = {
+			["added"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.2,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Aiming Station Left/Right (Joystick)",
+		},
+	},
+}
+return diff
