a0089323c80261f732adeef68c485191bc4b190f
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-07-07 19:05:40 -0300

    Mi24 ai mappings

---
+++ dcs_inputs/mi24_ai_stick.diff.lua
@@ -0,0 +1,45 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3001pnilu3001cd30vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN14",
+				},
+			},
+			["name"] = "Petrovitch AI Helper - SHOW/HIDE",
+		},
+		["d3002pnilu3002cd30vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN16",
+				},
+			},
+			["name"] = "Petrovitch Helper Interface - move RIGHT",
+		},
+		["d3003pnilu3003cd30vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN18",
+				},
+			},
+			["name"] = "Petrovitch Helper Interface - move LEFT",
+		},
+		["d3004pnilu3004cd30vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN15",
+				},
+			},
+			["name"] = "Petrovitch Helper Interface - move UP",
+		},
+		["d3005pnilu3005cd30vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN17",
+				},
+			},
+			["name"] = "Petrovitch Helper Intreface - move DOWN",
+		},
+	},
+}
+return diff
