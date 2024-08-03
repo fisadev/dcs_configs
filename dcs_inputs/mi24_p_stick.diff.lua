db16e79bf2249d4380a60f470fdec23c1badaf7c
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-08-03 20:34:59 -0300

    Fix inverted break axis

--- dcs_inputs/mi24_p_stick.diff.lua
+++ dcs_inputs/mi24_p_stick.diff.lua
@@ -61,6 +61,19 @@ local diff = {
 		["a2101cdnil"] = {
 			["added"] = {
 				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0,
+						},
+						["deadzone"] = 0,
+						["hardwareDetent"] = false,
+						["hardwareDetentAB"] = 0,
+						["hardwareDetentMax"] = 0,
+						["invert"] = true,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
 					["key"] = "JOY_SLIDER1",
 				},
 			},
