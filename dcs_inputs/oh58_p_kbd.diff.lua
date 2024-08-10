d02ac210d338cc47d773caecd8c2338f547c77b8
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-08-10 01:36:49 -0300

    Missing mappings

---
+++ dcs_inputs/oh58_p_kbd.diff.lua
@@ -0,0 +1,18 @@
+local diff = {
+	["keyDiffs"] = {
+		["d309pnilu0cd50vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Home",
+					["reformers"] = {
+						[1] = "LAlt",
+						[2] = "LCtrl",
+						[3] = "LShift",
+					},
+				},
+			},
+			["name"] = "Automatic Startup Procedure",
+		},
+	},
+}
+return diff
