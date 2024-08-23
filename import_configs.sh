1528a4cda45d183316ca95839947246f7444872a
Juan Pedro Fisanotti <fisadev@gmail.com>
2024-08-23 20:20:10 -0300

    No longer needed

--- import_configs.sh
+++ import_configs.sh
@@ -1,7 +1,6 @@
 #bash
 cp /c/Users/fisad/Saved\ Games/DCS/InputUserProfiles/*.* /d/flight/dcs_configs/dcs_inputs/
 cp /c/Users/fisad/Saved\ Games/DCS/Scripts/Export.lua /d/flight/dcs_configs/dcs_scripts/
-cp /c/Users/fisad/Desktop/remaps_dcs.ahk /d/flight/dcs_configs/autohotkey_scripts/
 cp /d/installed_aps/simpyt/simpyt_configs/* /d/flight/dcs_configs/simpyt_configs/ -r
 cp  "/c/Program Files (x86)/Steam/steamapps/common/DCSWorld/Mods/aircraft/F-15E/Cockpit/Scripts/ALE45_PRG.lua" /d/flight/dcs_configs/f15e_cockpit_scripts
 cp  "/c/Program Files (x86)/Steam/steamapps/common/DCSWorld/Scripts/DemoScenes/sceneVR.lua" /d/flight/dcs_configs/dcs_scripts
