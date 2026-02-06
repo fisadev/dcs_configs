1c0ce077d255f2e67c6e62c352ea16f21f198c18
Juan Pedro Fisanotti <fisadev@gmail.com>
2026-02-06 02:49:35 -0300

    Finally SSA working

--- dcs_scripts/Export.lua
+++ dcs_scripts/Export.lua
@@ -1,4 +1,8 @@
-dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
+-- simshaker with some weird hacks to prevent SSA 2.10.0 from breaking the config on boot
 local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua')
+dofile(SimShakerlfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
+dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
+-- tacview temporarily disabled as it breaks SSA 2.10.0
+-- local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
+-- srs
 pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Services\DCS-SRS\Scripts\DCS-SimpleRadioStandalone.lua]]); end,nil)
-local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
