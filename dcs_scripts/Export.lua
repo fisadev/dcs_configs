37dbcc1b226f7de90e51b9fea6315dcc21c1fd81
Juan Pedro Fisanotti <fisadev@gmail.com>
2026-02-12 23:52:18 -0300

    New export compatible with SSA 3.0

--- dcs_scripts/Export.lua
+++ dcs_scripts/Export.lua
@@ -1,8 +1,9 @@
--- simshaker with some weird hacks to prevent SSA 2.10.0 from breaking the config on boot
-local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua')
-dofile(SimShakerlfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
-dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]])
+-- simshaker
+pcall(function() local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua');
+dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]]); end, nil)
+
 -- tacview temporarily disabled as it breaks SSA 2.10.0
 -- local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
+
 -- srs
 pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Services\DCS-SRS\Scripts\DCS-SimpleRadioStandalone.lua]]); end,nil)
