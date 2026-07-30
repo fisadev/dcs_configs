4a0b41e1792375e0e25f66041c19bc0288d3fdcf
Juan Pedro Fisanotti <fisadev@gmail.com>
2026-07-29 23:19:35 -0300

    Export.lua updated with how SSA auto edits it

--- dcs_scripts/Export.lua
+++ dcs_scripts/Export.lua
@@ -1,9 +1,8 @@
 -- simshaker
-pcall(function() local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua');
-dofile(lfs.writedir()..[[Scripts\SimShaker-export-core\ExportCore.lua]]); end, nil)
-
 -- tacview temporarily disabled as it breaks SSA 2.10.0
+pcall(function() local SimShakerlfs=require('lfs'); dofile(SimShakerlfs.writedir()..'Scripts/SimShaker.lua'); end, nil)
 -- local Tacviewlfs=require('lfs');dofile(Tacviewlfs.writedir()..'Scripts/TacviewGameExport.lua')
-
 -- srs
+
 pcall(function() local dcsSr=require('lfs');dofile(dcsSr.writedir()..[[Mods\Services\DCS-SRS\Scripts\DCS-SimpleRadioStandalone.lua]]); end,nil)
+
